7e89ddf5c3e2b3092f57c0a130217ce4bcc1b9cd
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-03-29 11:10:59 +0630

    fb chat

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,64 @@ import MeetupList from "./../components/meetup/MeetupList";
 import { MongoClient } from "mongodb";
 import Head from "next/head";
 import useToken from "../lib/auth/useToken";
+import { useEffect } from "react";
 
 export default function Home({ meetups, data }) {
+  useEffect(() => {
+    const chatbox = document.getElementById("fb-customer-chat");
+    chatbox.setAttribute("page_id", "108012321864036");
+    chatbox.setAttribute("attribution", "biz_inbox");
+  }, []);
+
+  useEffect(() => {
+    window.fbAsyncInit = function () {
+      FB.init({
+        xfbml: true,
+        version: "v13.0",
+      });
+    };
+
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  }, []);
+
+  // <!-- Messenger Chat Plugin Code -->
+  //   <div id="fb-root"></div>
+
+  //   <!-- Your Chat Plugin code -->
+  //   <div id="fb-customer-chat" class="fb-customerchat">
+  //   </div>
+
+  //   <script>
+  //     var chatbox = document.getElementById('fb-customer-chat');
+  //     chatbox.setAttribute("page_id", "108012321864036");
+  //     chatbox.setAttribute("attribution", "biz_inbox");
+  //   </script>
+
+  //   <!-- Your SDK code -->
+  //   <script>
+  //     window.fbAsyncInit = function() {
+  //       FB.init({
+  //         xfbml            : true,
+  //         version          : 'v13.0'
+  //       });
+  //     };
+
+  //     (function(d, s, id) {
+  //       var js, fjs = d.getElementsByTagName(s)[0];
+  //       if (d.getElementById(id)) return;
+  //       js = d.createElement(s); js.id = id;
+  //       js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  //       fjs.parentNode.insertBefore(js, fjs);
+  //     }(document, 'script', 'facebook-jssdk'));
+  //   </script>
   const { token } = useToken();
   return (
     <>
@@ -15,6 +71,10 @@ export default function Home({ meetups, data }) {
         />
       </Head>
       <MeetupList meetups={meetups} token={token} />
+      <footer>
+        <div id="fb-root"></div>
+        <div id="fb-customer-chat" class="fb-customerchat"></div>
+      </footer>
     </>
   );
 }
